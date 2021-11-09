5914a56262e78c311be9f085940810e1615db2c8
Saatvik Nagpal <saatvikngpl21@gmail.com>
2021-11-09 18:14:20 +0530

    day 2 build

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,17 @@
 import 'tailwindcss/tailwind.css'
+import '../styles/global.css'
+import Router from 'next/router';
+import ProgressBar from '@badrap/bar-of-progress'
+
+const progress = new ProgressBar({
+  size:4,
+  color: "#FE595E",
+  className:"bar-of-progress",
+  delay: 100,
+});
+
+Router.events.on('routeChangeStart', progress.start)
+Router.events.on('routeChangeComplete', progress.finish)
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
